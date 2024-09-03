19918be6c152799a1017098fed64018056584874
Kwon Zhu <kwonzhu@gmail.com>
2024-09-03 22:00:09 +0930

    creat my first react app

---
+++ FrontEndLearning/React/my-first-react-app/src/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello, welcome to My Frist App!</h2>
+      <p>This is a simple React component</p>
+    </div>
+  );
+}
+export default Greeting;
