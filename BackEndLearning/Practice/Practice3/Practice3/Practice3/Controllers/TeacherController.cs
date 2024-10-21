418386a8ea016bfcbf6725a849740ce46678c778
Kwon Zhu <kwonzhu@gmail.com>
2024-10-21 22:22:48 +1030

    RESTful API for TeacherController

--- BackEndLearning/Practice/Practice3/Practice3/Practice3/Controllers/TeacherController.cs
+++ BackEndLearning/Practice/Practice3/Practice3/Practice3/Controllers/TeacherController.cs
@@ -1,6 +1,18 @@
-﻿namespace Practice3.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice3.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice3.Controllers
 {
-    public class TeacherController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeacherController : ControllerBase
     {
+        List<Teacher> teachers = new List<Teacher>()
+        {
+            new Teacher {UserId = 1, Department = "FrontEnd", Description = "FrontEnd teacher", Specialty = "javascript" },
+            new Teacher { UserId = 2, Department = "BackEnd", Description = "BackEnd teacher", Specialty = "c#" },
+            new Teacher { UserId = 3, Department = "FrontEnd", Description = "FrontEnd teacher", Specialty = "react" },
+        };
     }
 }
