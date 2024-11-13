605a0d96aee890dcfd83396efcf89191dcb72d03
Kwon Zhu <kwonzhu@gmail.com>
2024-11-13 10:50:29 +1030

    add UserProfilePage and NavBar, create Nav Links, now clicks NavBar items can redirect to different pages

--- FrontEndLearning/routing-contextapi-practice/src/App.tsx
+++ FrontEndLearning/routing-contextapi-practice/src/App.tsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CourseListPage from './pages/CourseListPage';
+import NavBar from './components/NavBar';
+import UserProfilePage from './pages/UserProfilePage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/courses" element={<CourseListPage />} />
-    </Routes>
+    <div>
+      <NavBar />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses" element={<CourseListPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
